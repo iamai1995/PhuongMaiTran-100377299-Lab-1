1578395047ae5d9b3875d429b54013e6e696510d
Phuong Mai Tran <ptran20@mylangara.ca>
2022-05-21 15:16:08 -0700

    Fourth version

---
+++ src/hello_world.py
@@ -0,0 +1,13 @@
+'''
+My first program in Python
+Phuong Mai Tran
+ID: 100377299
+'''
+import math
+print('Hello, world!')
+name = ''
+name = input('Please enter your name: ')
+print('Welcome ' + name.capitalize() + ' to Computing for data Analytics!')
+print(f'Welcome {name.title()} to Computing for Data Analytics!')
+x = math.pi
+print(f'The value of PI with nine decimal places is {x:.9F}')
